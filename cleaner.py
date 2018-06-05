08afdb466554144e2eaa22a11edcbe3bf0b12fe6
Kaptijn Dijlandt <dylanjoepprins@gmail.com>
2018-06-05 15:05:23 +0200

    Cleaned

--- cleaner.py
+++ cleaner.py
@@ -1,1 +1,12 @@
-import CSV
+import csv
+#with open('DataBase2018.csv', 'r') as bestand:
+#    reader = csv.reader(bestand)
+#    for row in reader:
+#        for data in row:
+#            if data == 'GAS':
+#                writer.writerow(row)
+with open('DataBase2018.csv', 'r') as inp, open('editDataBase2018.csv', 'w') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[8] != 'GAS':
+            writer.writerow(row)
