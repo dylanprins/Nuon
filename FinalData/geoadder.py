e7a5edcd1af36ecf954cfeb3c10da50e293f50e2
rowangroeneveld <33287066+rowangroeneveld@users.noreply.github.com>
2018-06-05 23:17:56 +0200

    big update

--- FinalData/geoadder.py
+++ FinalData/geoadder.py
@@ -1,5 +1,5 @@
 import csv
-with open('editDataBase2009.csv', 'r') as inp, open('postcodetabelgeo.csv', 'r') as geos:
+with open('editDataBase2016.csv', 'r') as inp, open('postcodetabelgeo.csv', 'r') as geos:
     lst1 = []
     lst2 = []
     for row in csv.reader(inp):
@@ -7,16 +7,17 @@ with open('editDataBase2009.csv', 'r') as inp, open('postcodetabelgeo.csv', 'r')
     for line in csv.reader(geos):
         lst2 += [line]
 categories = lst1[0] + ['longitude', 'latitude']
-with open('Data2009.txt', 'w') as out:
+with open('Data2016.txt', 'w') as out:
     writer = csv.writer(out)
     writer.writerow(categories)
     for i in lst1:
         for j in lst2:
             if i[5] == j[0]:
-                i[:-2] = [j[3]]
-                i[:-1] = [j[4]]
-                writer.writerow(i)
-with open('Data2009.txt') as inp, open("Data2009.csv", "w") as out:
+                i += [j[3]]
+                i += [j[4]]
+                if len(i) == 22:
+                    writer.writerow(i)
+with open('Data2016.txt') as inp, open("Data2016.csv", "w") as out:
     lines, mem = inp.readlines(), ''
     for line in lines:
         if line == '\n':
--- plotter.py
+++ plotter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("FinalData\Data2018.csv")
+data = pd.read_csv("FinalData\Data2009.csv")
 
-print(data[21])
+print(data["POSTCODE_TOT"])
