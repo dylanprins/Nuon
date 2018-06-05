7d0389ff50512ab67617449c67eef74ae22bda8c
rowangroeneveld <33287066+rowangroeneveld@users.noreply.github.com>
2018-06-05 14:24:46 +0200

    niet testje

--- testje.php
+++ testje.php
@@ -7,4 +7,35 @@
 	echo 'Latitude:' . $coordinates->results[0]->geometry->location->lat;
 	echo 'Longitude:' . $coordinates->results[0]->geometry->location->lng;
  
+	$lat = $coordinates->results[0]->geometry->location->lat;
+	$lng = $coordinates->results[0]->geometry->location->lng;
+ 
 ?>
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Localizing the Map</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=en"></script>
+    <script>
+		function initialize() {
+		  var mapOptions = {
+			zoom: 15,
+			center: new google.maps.LatLng('<?php echo $lat ?>', '<?php echo $lng ?>')
+		  };
+		  var map = new google.maps.Map(document.getElementById('map-canvas'),
+			  mapOptions);
+		}
+ 
+		google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
