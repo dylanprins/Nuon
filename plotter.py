dc09f8b201b11e6f0028863251cbcf30b941a990
rowangroeneveld <33287066+rowangroeneveld@users.noreply.github.com>
2018-06-06 01:05:11 +0200

    minor update

--- plotter.py
+++ plotter.py
@@ -9,15 +9,11 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 
 for i in range(16549):
-    # print(data["longitude"][i])
-    # print(data["latitude"][i])
     if data["longitude"][i] < 52.424294 and data["longitude"][i] > 52.318159:
         if data["latitude"][i] > 4.812657 and data["latitude"][i] < 4.988321:
             y.append(data["longitude"][i])
             x.append(data["latitude"][i])
 
-print(y)
-print(x)
 ax.plot(x, y, marker = '.', markersize = '0.3', linestyle='None', color = "red")
 x0, x1 = ax.get_xlim()
 y0, y1 = ax.get_ylim()
