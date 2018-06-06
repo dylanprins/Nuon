687a5d7cd810ab3f129e2c75db78ef5e4d66b4b3
rowangroeneveld <33287066+rowangroeneveld@users.noreply.github.com>
2018-06-06 10:43:10 +0200

    d

--- colorplotter.py
+++ colorplotter.py
@@ -16,14 +16,20 @@ for i in range(16549):
             y.append(data["longitude"][i])
             x.append(data["latitude"][i])
             temp = np.array([data["SJV"][i]])
+            # sizetemp = np.array((data["SJV"][i]/20000) ** 1.2)
+           
+            # size = np.concatenate([t, sizetemp])
             t = np.concatenate([t, temp])
             
+            
 print(t)
-ax.scatter(x, y, c=t, cmap='gist_stern', s= 0.3)
+ax.scatter(x, y, c=t, cmap='tab20c', s=(t/20000)**3, alpha = 0.8, marker=r'$\odot$')
             # ax.plot(x, y, marker = '.', markersize = '0.3', linestyle='None', color = "red")
-        
-x0, x1 = ax.get_xlim()
-y0, y1 = ax.get_ylim()
+
+x0, x1 = 4.812657, 4.988321
+y0, y1 = 52.318159, 52.424294        
+# x0, x1 = ax.get_xlim()
+# y0, y1 = ax.get_ylim()
 img = plt.imread("amsterdamhdmap.png")
 ax.imshow(img, extent = [x0, x1, y0, y1], aspect = "auto", zorder = 0)
-plt.savefig("testplot1.png", dpi = 900)
+plt.savefig("testplot1.png", dpi = 1300)
