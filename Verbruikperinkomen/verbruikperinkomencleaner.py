e1892da5dd0e0d27b6a95a4140259d4ee904aabe
Kaptijn Dijlandt <dylanjoepprins@gmail.com>
2018-06-08 15:59:00 +0200

    Code verbeterd maar nog niet af

--- Verbruikperinkomen/verbruikperinkomencleaner.py
+++ Verbruikperinkomen/verbruikperinkomencleaner.py
@@ -2,17 +2,28 @@ import csv
 with open('../DataBases/editDatabase2018.csv', 'r') as inp, open('witgewasseninkomen.csv', 'r') as sinp, open('verbruik per inkomen.csv', 'w') as out:
     writer = csv.writer(out)
     c=0
+    oudverbruik = 0
+    totaalverbruik=0
     inkomens = csv.reader(sinp)
     for row in csv.reader(inp):
-        postcode= row[4]
-        if oudverbruik == 0:
-            oudverbruik= row[16]
-        if oudverbruik != 0:
-            nieuwverbruik=row[16]
-            totaalverbruik = oudverbruik+nieuwverbruik
-        c+=1
-        print(postcode)
-        print(verbruik)
+        if c==0:
+            c+=1
+        elif c==1:
+            if oudverbruik == 0:
+                oudverbruik= row[16]
+                print(oudverbruik)
+                postcode=row[4]   
+            if oudverbruik != 0:
+                newpostcode=row[4]
+                if newpostcode[0:4] == postcode[0:4]:
+                    nieuwverbruik=row[16]
+                    totaalverbruik = oudverbruik+nieuwverbruik
+                    oudverbruik= totaalverbruik
+                    postcode = row[4]
+                else:
+                    totaalverbruik = 0
+                    oudverbruik = 0
+
     for line in inkomens:
         postcodeinkomen= line[0]
         inkomen = line[2]
