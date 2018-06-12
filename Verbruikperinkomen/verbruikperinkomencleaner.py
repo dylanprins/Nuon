92448962a0f4ee80307d5c821a6b5e5666ba213c
Kaptijn Dijlandt <dylanjoepprins@gmail.com>
2018-06-12 13:50:26 +0200

    tussenstand

--- Verbruikperinkomen/verbruikperinkomencleaner.py
+++ Verbruikperinkomen/verbruikperinkomencleaner.py
@@ -4,25 +4,29 @@ with open('../DataBases/editDatabase2018.csv', 'r') as inp, open('witgewassenink
     c=0
     oudverbruik = 0
     totaalverbruik=0
+    length=0
     inkomens = csv.reader(sinp)
+    numlines = len(inp.readlines())
+    print(numlines)
     for row in csv.reader(inp):
         if c==0:
             c+=1
-        elif c==1:
+        elif c!=0:
+            c+=1
             if oudverbruik == 0:
-                oudverbruik= row[16]
-                print(oudverbruik)
+                oudverbruik= int(row[16])
                 postcode=row[4]   
             if oudverbruik != 0:
                 newpostcode=row[4]
                 if newpostcode[0:4] == postcode[0:4]:
-                    nieuwverbruik=row[16]
-                    totaalverbruik = oudverbruik+nieuwverbruik
+                    nieuwverbruik=int(row[16])
+                    oudverbruik = int(oudverbruik+nieuwverbruik)
                     postcode = row[4]
                 else:
-                    totaalverbruik = 0
-                    oudverbruik = 0
-
+                    print(oudverbruik)
+                    oudverbruik = int(row[16])
+                    postcode = newpostcode
+    print(c)
     for line in inkomens:
         postcodeinkomen= line[0]
         inkomen = line[2]
