23b343642f1c771a18f8ede1aeca947979034e6c
Kaptijn Dijlandt <dylanjoepprins@gmail.com>
2018-06-12 18:15:46 +0200

    JAAAA EINDELIJKKK!!!

--- Verbruikperinkomen/verbruikperinkomencleaner.py
+++ Verbruikperinkomen/verbruikperinkomencleaner.py
@@ -1,13 +1,30 @@
 import csv
+import pandas as pd
+
+newrow = []
+def appendinkomen(postcode):
+    for line in inkomens:
+        if line[0]==postcode[0:4]:
+            moneys = line[2]
+            newrow.append(postcode[0:4])
+            newrow.append(oudverbruik)
+            newrow.append(moneys)
+            writer.writerow(line)
+            newrow.clear()
+
 with open('../DataBases/editDatabase2018.csv', 'r') as inp, open('witgewasseninkomen.csv', 'r') as sinp, open('verbruik per inkomen.csv', 'w') as out:
     writer = csv.writer(out)
+    writer.writerow(['Postcode','Verbruik','Inkomen'])
     c=0
     oudverbruik = 0
     totaalverbruik=0
     length=0
     inkomens = csv.reader(sinp)
-    numlines = len(inp.readlines())
-    print(numlines)
+    numlines = len(pd.read_csv('../DataBases/editDatabase2018.csv'))
+    newrow=[]
+
+
+
     for row in csv.reader(inp):
         if c==0:
             c+=1
@@ -18,15 +35,20 @@ with open('../DataBases/editDatabase2018.csv', 'r') as inp, open('witgewassenink
                 postcode=row[4]   
             if oudverbruik != 0:
                 newpostcode=row[4]
-                if newpostcode[0:4] == postcode[0:4]:
+                if c == numlines+1:
+                    sinp.seek(0)
+                    appendinkomen(postcode)
+                    c=0
+                elif newpostcode[0:4] == postcode[0:4]:
                     nieuwverbruik=int(row[16])
                     oudverbruik = int(oudverbruik+nieuwverbruik)
                     postcode = row[4]
                 else:
-                    print(oudverbruik)
+                    sinp.seek(0)
+                    appendinkomen(postcode)
                     oudverbruik = int(row[16])
                     postcode = newpostcode
-    print(c)
-    for line in inkomens:
-        postcodeinkomen= line[0]
-        inkomen = line[2]
+
+
+
+            
